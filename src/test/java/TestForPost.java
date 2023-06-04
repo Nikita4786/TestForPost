1a1d45b86251c993de5eb1ddc6d1d85664561f3d
Nikita <grigoryantsnm@gmail.com>
2023-06-04 18:57:09 +0300

    first commit

---
+++ src/test/java/TestForPost.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class TestForPost {
+    @Test
+    void TestsPost() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data",equalTo("some data"))
+        ;
+    }
+}
